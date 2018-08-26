3ac212306f1d9cf1c7b21d1a7c8e9a60fbab13d3
LuK1337 <priv.luk@gmail.com>
2018-08-26 15:00:21 +0200

    Settings: Add tequilaOS entries into device info
    Author: Harry Youd <harry@harryyoud.co.uk>
    Date:   Fri Nov 24 09:49:51 2017 -0800
    
        Settings: Add LineageOS version and build date to device info
    
        Change-Id: I4acf834442712954f8e9db15370bffafd4547174
    
    Author: Kevin F. Haggerty <haggertk@lineageos.org>
    Date:   Sun Mar 20 11:40:54 2022 -0600
    
        Settings: Use display version for, well, displayed version
    
        Change-Id: I335b4467721fdbc6ccc2e6a7d8ab210868cd0d54
    
    Change-Id: I8d4c6869f8dfa34a96567bec18f9f8276b66a64e

---
+++ src/com/android/settings/deviceinfo/firmwareversion/tequilaVersionDetailPreferenceController.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.SystemClock;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.VisibleForTesting;
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.core.BasePreferenceController;
+import com.android.settingslib.RestrictedLockUtils;
+import com.android.settingslib.RestrictedLockUtilsInternal;
+
+public class tequilaVersionDetailPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "tequilaVersionDialogCtrl";
+
+    private static final String KEY_TEQUILA_VERSION_PROP = "ro.tequila.display.version";
+
+    public tequilaVersionDetailPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public boolean useDynamicSliceSummary() {
+        return true;
+    }
+
+    @Override
+    public boolean isSliceable() {
+        return true;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return SystemProperties.get(KEY_TEQUILA_VERSION_PROP,
+                mContext.getString(R.string.unknown));
+    }
+}
