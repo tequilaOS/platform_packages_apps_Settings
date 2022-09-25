bb086ddf9eea7360240a659f82a59b962889b6f7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-25 19:02:56 +0530

    Revert "Add setting for showing the vibrate icon in status bar"
    We already have statusbar tuner for it.
    
    This reverts commit 16148d2c8cfc53e92ba966a1e06942a38779cd9d.
    
    Change-Id: I8f4b59d05a628cd9708c871374632656b507eb47

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -267,8 +267,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new BootSoundPreferenceController(context);
         final EmergencyTonePreferenceController emergencyTonePreferenceController =
                 new EmergencyTonePreferenceController(context, fragment, lifecycle);
-        final VibrateIconPreferenceController vibrateIconPreferenceController =
-                new VibrateIconPreferenceController(context, fragment, lifecycle);
         final LinkedVolumesPreferenceController linkedVolumesPreferenceController =
                 new LinkedVolumesPreferenceController(context, fragment, lifecycle);
 
@@ -277,7 +275,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         controllers.add(chargingSoundPreferenceController);
         controllers.add(dockingSoundPreferenceController);
         controllers.add(touchSoundPreferenceController);
-        controllers.add(vibrateIconPreferenceController);
         controllers.add(dockAudioMediaPreferenceController);
         controllers.add(bootSoundPreferenceController);
         controllers.add(emergencyTonePreferenceController);
@@ -289,7 +286,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                         chargingSoundPreferenceController,
                         dockingSoundPreferenceController,
                         touchSoundPreferenceController,
-                        vibrateIconPreferenceController,
                         dockAudioMediaPreferenceController,
                         bootSoundPreferenceController,
                         emergencyTonePreferenceController,
