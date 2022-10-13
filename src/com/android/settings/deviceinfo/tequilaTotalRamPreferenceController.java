3a6028cae0fb06c4aafab6542a83a9048625167a
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-10-13 11:12:06 +0200

    Settings: Show Total Ram in About Phone page
    Change-Id: I33f06f9e4a4e045495151e09acd16fab2e67042f

---
+++ src/com/android/settings/deviceinfo/tequilaTotalRamPreferenceController.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2022 tequilaOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.SystemClock;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.text.TextUtils;
+import android.text.format.Formatter;
+import android.util.Log;
+
+import androidx.annotation.VisibleForTesting;
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.core.BasePreferenceController;
+import com.android.settingslib.RestrictedLockUtils;
+import com.android.settingslib.RestrictedLockUtilsInternal;
+
+public class tequilaTotalRamPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "tequilaTotalRamCtrl";
+
+    public tequilaTotalRamPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        MemInfo memInfo = mStatsManager.getMemInfo();
+
+        double totalRam = memInfo.realTotalRam;
+
+        String tequilaTotalRam = Formatter.formatShortFileSize(context, (long) totalRam);
+
+        if (!tequilaTotalRam.isEmpty()) {
+            return tequilaTotalRam;
+        } else {
+            return mContext.getString(R.string.unknown);
+        }
+    }
+}
